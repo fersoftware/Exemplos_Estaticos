7b0e7d2bd96852d57e450c891ee24538c3d34d98
FERNANDO ALVES DA SILVA <37001@alunos.bandtec.com.br>
2016-04-15 19:48:06 -0300

    Aprendendo Statics

---
+++ Exemplos_Estaticos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplos_Estaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // não posso instanciar esse objeto
+            //Conversao objConv = new Conversao();
+        }
+    }
+}
